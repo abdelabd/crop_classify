f3754742ecab63afb9a309de258c3ac3a71b38c2
Abdel Elabd <abdelabd777@gmail.com>
2024-10-11 10:52:13 -0700

    fixed bug: translate to 1D/contiguous arrays

--- custom_hls_layer/test_conversions/nnet_crop.h
+++ custom_hls_layer/test_conversions/nnet_crop.h
@@ -34,16 +34,16 @@ struct crop_config {
 // Let's do everything in rolled-up form for now
 template <class data1_T, class data2_T, class res_T, typename CONFIG_T>
 void crop(
-    data1_T image[CONFIG_T::in_height][CONFIG_T::in_width][CONFIG_T::n_chan],
-    data2_T crop_coordinates_normed[CONFIG_T::n_crop_boxes][4], //_normed as in normalized to the image size
-    res_T cropped_images[CONFIG_T::n_crop_boxes][CONFIG_T::crop_rows][CONFIG_T::crop_cols][CONFIG_T::n_chan]) {
+    data1_T image[CONFIG_T::in_height*CONFIG_T::in_width*CONFIG_T::n_chan],
+    data2_T crop_coordinates_normed[CONFIG_T::n_crop_boxes*4], //_normed as in normalized to the image size
+    res_T cropped_images[CONFIG_T::n_crop_boxes*CONFIG_T::crop_rows*CONFIG_T::crop_cols*CONFIG_T::n_chan]) {
     // #pragma HLS INLINE region Let's avoid pragmas for now
 
     for (unsigned box_idx = 0; box_idx < CONFIG_T::n_crop_boxes; box_idx++) {
-        data2_T y1_normed = crop_coordinates_normed[box_idx][0];
-        data2_T x1_normed = crop_coordinates_normed[box_idx][1];
-        data2_T y2_normed = crop_coordinates_normed[box_idx][2];
-        data2_T x2_normed = crop_coordinates_normed[box_idx][3];
+        data2_T y1_normed = crop_coordinates_normed[box_idx*4+0];
+        data2_T x1_normed = crop_coordinates_normed[box_idx*4+1];
+        data2_T y2_normed = crop_coordinates_normed[box_idx*4+2];
+        data2_T x2_normed = crop_coordinates_normed[box_idx*4+3];
         unsigned y1 = y1_normed * CONFIG_T::in_height;
         unsigned x1 = x1_normed * CONFIG_T::in_width;
         unsigned y2 = y2_normed * CONFIG_T::in_height;
@@ -54,11 +54,18 @@ void crop(
 
             unsigned dest_col = 0;
             for (unsigned src_col = y1; src_col < y2; src_col++) {
-                for (unsigned chan = 0; chan < CONFIG_T::n_chan; chan++) {
-                    cropped_images[box_idx][dest_row][dest_col][chan] = image[src_row][src_col][chan];
+
+                unsigned dest_chan = 0;
+                for (unsigned src_chan = 0; src_chan < CONFIG_T::n_chan; src_chan++) {
+
+                    unsigned src_idx = src_row*CONFIG_T::in_width*CONFIG_T::n_chan + src_col*CONFIG_T::n_chan + src_chan;
+                    unsigned dest_idx = box_idx*CONFIG_T::crop_rows*CONFIG_T::crop_cols*CONFIG_T::n_chan + dest_row*CONFIG_T::crop_cols*CONFIG_T::n_chan + dest_col*CONFIG_T::n_chan + dest_chan;
+                    cropped_images[dest_idx] = image[src_idx];
+
+                    dest_chan += 1;
                 }
+
                 dest_col += 1;
-                nnet::log_variable("dest_col", dest_col);
             }
 
             dest_row += 1;
