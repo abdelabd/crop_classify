ccdf6e00dc70d5c8e15429ddffac699d177f1dec
Abdel Elabd <abdelabd777@gmail.com>
2024-10-24 13:02:43 -0700

    softcode index-type

--- custom_hls_layer/test_conversions/nnet_crop.h
+++ custom_hls_layer/test_conversions/nnet_crop.h
@@ -21,7 +21,7 @@ struct crop_config {
 };
 
 // Let's do everything in rolled-up form for now
-template <class data1_T, class data2_T, class res_T, typename CONFIG_T>
+template <class data1_T, class data2_T, class index_T, class res_T, typename CONFIG_T>
 void crop(
     data1_T image[CONFIG_T::in_height*CONFIG_T::in_width*CONFIG_T::n_chan],
     data2_T crop_coordinates_normed[CONFIG_T::n_crop_boxes*4], //_normed as in normalized to the image size
@@ -35,25 +35,25 @@ void crop(
         data2_T x1_normed = crop_coordinates_normed[box_idx*4+1];
         data2_T y2_normed = crop_coordinates_normed[box_idx*4+2];
         data2_T x2_normed = crop_coordinates_normed[box_idx*4+3];
-        ap_uint<13> y1 = y1_normed * CONFIG_T::in_height; // TODO: softcode index type/width, can base it on image size (should do this on frontend)
-        ap_uint<13> x1 = x1_normed * CONFIG_T::in_width;
-        ap_uint<13> y2 = y2_normed * CONFIG_T::in_height;
-        ap_uint<13> x2 = x2_normed * CONFIG_T::in_width;
+        index_T y1 = y1_normed * CONFIG_T::in_height; // TODO: softcode index type/width, can base it on image size (should do this on frontend)
+        index_T x1 = x1_normed * CONFIG_T::in_width;
+        index_T y2 = y2_normed * CONFIG_T::in_height;
+        index_T x2 = x2_normed * CONFIG_T::in_width;
 
-        ap_uint<13> src_row = y1;
-        for (ap_uint<13> dest_row = 0; dest_row < CONFIG_T::crop_rows; dest_row++) {
+        index_T src_row = y1;
+        for (index_T dest_row = 0; dest_row < CONFIG_T::crop_rows; dest_row++) {
             // #pragma HLS UNROLL // TODO: Change to pipeline. If no pragma, then automatically pipelines
 
-            ap_uint<13> src_col = x1;
-            for (ap_uint<13> dest_col=0; dest_col < CONFIG_T::crop_cols; dest_col++ ){
+            index_T src_col = x1;
+            for (index_T dest_col=0; dest_col < CONFIG_T::crop_cols; dest_col++ ){
                 #pragma HLS UNROLL // TODO: Change to pipeline
 
-                ap_uint<13> src_chan = 0;
-                for (ap_uint<13> dest_chan = 0; dest_chan < CONFIG_T::n_chan; dest_chan++) {
+                index_T src_chan = 0;
+                for (index_T dest_chan = 0; dest_chan < CONFIG_T::n_chan; dest_chan++) {
                     // #pragma HLS UNROLL // TODO: Try to remove this
                 
-                    ap_uint<13> src_idx = src_row*CONFIG_T::in_width*CONFIG_T::n_chan + src_col*CONFIG_T::n_chan + src_chan;
-                    ap_uint<13> dest_idx = box_idx*CONFIG_T::crop_rows*CONFIG_T::crop_cols*CONFIG_T::n_chan + dest_row*CONFIG_T::crop_cols*CONFIG_T::n_chan + dest_col*CONFIG_T::n_chan + dest_chan;
+                    index_T src_idx = src_row*CONFIG_T::in_width*CONFIG_T::n_chan + src_col*CONFIG_T::n_chan + src_chan;
+                    index_T dest_idx = box_idx*CONFIG_T::crop_rows*CONFIG_T::crop_cols*CONFIG_T::n_chan + dest_row*CONFIG_T::crop_cols*CONFIG_T::n_chan + dest_col*CONFIG_T::n_chan + dest_chan;
                     cropped_images[dest_idx] = image[src_idx];
                     src_chan += 1;
                 }
